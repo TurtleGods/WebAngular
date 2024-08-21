b8b056aa37e235d9a99254220a137a74df8b2b6f
TurtleGods <strollist@gmail.com>
2024-08-22 00:27:24 +0800

    [feat] 加入正確routing 現在routing 錯誤會回到login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register/register.component';
 import { MainComponent } from './main/main.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    {path:'main',component:MainComponent},
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: 'main', component: MainComponent },
+    { path: '**', redirectTo: '/login' }
 ];
