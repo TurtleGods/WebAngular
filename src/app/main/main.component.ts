4fd73835c7f6f9ef820e8fdb102d35cae138cf21
Turtlegods <strollist@gmail.com>
2024-08-26 13:47:23 +0800

    [feat] 加入top bar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { MyCalendarComponent } from "./my-calendar/my-calendar.component";
+import { TopbarComponent } from "./topbar/topbar.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent, MyCalendarComponent],
+  imports: [FooterComponent, MyCalendarComponent, TopbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
