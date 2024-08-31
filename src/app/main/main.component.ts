78758ec570a0aa33340d129964a976bca189e0aa
TurtleGods <strollist@gmail.com>
2024-08-31 16:09:46 +0800

    [feat] 加入沒功能的topbar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { CustomCalendarComponent } from './custom-calendar/custom-calendar.component';
+import { TopbarComponent } from './topbar/topbar.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent,CustomCalendarComponent],
+  imports: [FooterComponent,CustomCalendarComponent,TopbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
