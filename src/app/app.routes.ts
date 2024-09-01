1d1f30fd55ee52931049aa4b2c6e9fd406bba4cc
Turtlegods <strollist@gmail.com>
2024-09-01 17:44:13 +0800

    [feat] 加入main page的router 原來routes可以重複使用，很強

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,23 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register/register.component';
 import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './main/home/home.component';
+import { MyCalendarComponent } from './main/my-calendar/my-calendar.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'main', component: MainComponent },
-    { path: '**', redirectTo: '/login' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'favorites', component: HomeComponent },
+      { path: 'schedule', component: MyCalendarComponent },
+      { path: 'records', component: MyCalendarComponent  },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route for /main
+    ],
+  },
+  { path: '**', redirectTo: '/login' },
 ];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { MyCalendarComponent } from "./my-calendar/my-calendar.component";
 import { TopbarComponent } from "./topbar/topbar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent, MyCalendarComponent, TopbarComponent],
+  imports: [FooterComponent, MyCalendarComponent, TopbarComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
