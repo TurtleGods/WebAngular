101cb20c68338ff8fe86992a6b0e7f8095125220
Turtlegods <strollist@gmail.com>
2024-09-01 18:06:40 +0800

    [feat] 加入homepage search bar

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  searchQuery: string = '';
 
+  search(): void {
+    // Implement search logic here
+    console.log('Search query:', this.searchQuery);
+  }
 }
