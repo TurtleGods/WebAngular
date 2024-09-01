ddd97439e6326889234c34ccf3723bac60ce93bd
Turtlegods <strollist@gmail.com>
2024-09-01 15:12:31 +0800

    [feat] 加入Top bar happyGym Title

--- src/app/main/topbar/topbar.component.ts
+++ src/app/main/topbar/topbar.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
 export class TopbarComponent {
+  selectedTitle: string = 'HappyGym';
+  dropdownOpen: boolean = false;
 
+  selectTitle(title: string) {
+    this.selectedTitle = title;
+    this.dropdownOpen = false;
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
